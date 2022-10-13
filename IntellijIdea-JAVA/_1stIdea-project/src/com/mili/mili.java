cb35dc8c06330a653bb79124e4d302bd44eee003
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2022-10-13 17:24:40 +0530

    just some editing

--- IntellijIdea-JAVA/_1stIdea-project/src/com/mili/mili.java
+++ IntellijIdea-JAVA/_1stIdea-project/src/com/mili/mili.java
@@ -1,6 +1,7 @@
-package com.mili
+package com.mili;
 public class mili {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("nothign much");
     }
 }
