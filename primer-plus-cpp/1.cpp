d26d640d9c181a9e6d8746dd1e7a0f519d7a4d18
LegioN2004 <sunnybaruasins@gmail.com>
2023-01-31 23:09:19 +0530

    primer plus book sht

---
+++ primer-plus-cpp/1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "this is the first output from the first example of the book";
+  cout << endl;
+  cout << "this is the second output from the first example of the book";
+  cin.get();
+  /* return 0; */
+}
