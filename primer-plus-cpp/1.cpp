6b930200942772cfa98fb050ffc10c4ed2cfd89a
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-02-01 22:30:39 +0530

    nnn

--- primer-plus-cpp/1.cpp
+++ primer-plus-cpp/1.cpp
@@ -5,6 +5,6 @@ int main() {
   cout << "this is the first output from the first example of the book";
   cout << endl;
   cout << "this is the second output from the first example of the book";
-  cin.get();
-  /* return 0; */
+  // cin.get();
+  return 0; 
 }
