6066b89fe66c6b0789a4b2ce9f5be8d86c52c8e3
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-02-01 22:46:17 +0530

    artart

--- primer-plus-cpp/1.cpp
+++ primer-plus-cpp/1.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main() {
   cout << "this is the first output from the first example of the book";
+  printf("tis is nothign");
   cout << endl;
   cout << "this is the second output from the first example of the book";
   // cin.get();
