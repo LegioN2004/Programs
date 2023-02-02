3a641eb2acad58ef32c88fac6bb58f1dfb739b47
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-02-02 22:52:05 +0530

    nothing much

--- primer-plus-cpp/1.cpp
+++ primer-plus-cpp/1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
   cout << "this is the first output from the first example of the book";
   printf("tis is nothign");
-  cout << endl;
+  cout << enl;
   cout << "this is the second output from the first example of the book";
   // cin.get();
   return 0; 
