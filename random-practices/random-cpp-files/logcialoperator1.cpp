587feef931aca95aa2d397028600ecf1eeab7df1
LegioN2004 <sunnybaruasins@gmail.com>
2023-02-22 13:00:28 +0530

    aosnt

--- random-practices/random-cpp-files/logcialoperator1.cpp
+++ random-practices/random-cpp-files/logcialoperator1.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
   int a = 1, b = 2, c = 3, d = 4, e = 5;
   cout << ((a > b) && (b > a)) << "\n";
+  cout << ((a > b) && (b > a) && (a > c)) << "\n";
+  cout << ((b > a) && (c > a) && (e > c)) << "\n";
   cout << ((b > c) || (b > a)) << "\n";
   cout << (!(c > b));
 }
