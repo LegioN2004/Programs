b7a27fc0843dbfcb428fc0781f3c8719c59be347
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-03-11 16:24:15 +0530

    niooinn

--- random-practices/random-cpp-files/fibonaccitesting.cpp
+++ random-practices/random-cpp-files/fibonaccitesting.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
+#include<conio>
 using namespace std;
 
 int main(){
    int a,b,c;
-   cout<<"enter the number";
-   cin>>a>>b;
-   c=a+b;
+   cout << "enter the number";
+   cin >> a >> b;
+   c = a+b;
    cout << "is the required number" << c;
    return 0;
 }
