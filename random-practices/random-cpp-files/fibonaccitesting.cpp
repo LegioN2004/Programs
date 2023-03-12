9043fa030e3bf65b6dffa63b6fe79af605ed98cb
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-03-12 10:40:22 +0530

    artrt

--- random-practices/random-cpp-files/fibonaccitesting.cpp
+++ random-practices/random-cpp-files/fibonaccitesting.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<conio>
+#include<conio.h>
 using namespace std;
 
 int main(){
--- random-practices/random-cpp-files/function1.cpp
+++ random-practices/random-cpp-files/function1.cpp
@@ -3,12 +3,14 @@ using namespace std;
 
 int Multiply(int c, int d) { return c * d; }
 
-void MultiplyAndChange(int a, int b) {
+void MultiplyAndChange(int a, int b)
+{
   int multiply = Multiply(a, b);
   cout << multiply << endl;
 }
 
-int main() {
+int main()
+{
   MultiplyAndChange(10, 10);
   MultiplyAndChange(0, 0);
   MultiplyAndChange(1, 2);
@@ -16,6 +18,8 @@ int main() {
   int multiply = Multiply(9, 8);
   int multiply1 = Multiply(8, 8);
   int multiply2 = Multiply(7, 8);
-  cout << multiply << "\n" << multiply1 << "\n" << multiply2;
+  cout << multiply << "\n"
+       << multiply1 << "\n"
+       << multiply2;
   return 0;
 }
