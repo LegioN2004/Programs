95e09470c480e11b70b73e56aa4502cdfb60f4e7
LegioN2004 <sunnybaruasins@gmail.com>
2023-03-14 10:05:28 +0530

    ona

--- JAVA/DSA-bootcamp-kunal-kush/lecture1/Main.java
+++ JAVA/DSA-bootcamp-kunal-kush/lecture1/Main.java
@@ -2,12 +2,5 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("idea starts way faster in arch linux but not in windows");
-	}public class
-
-	void Main{
-
-	public static void main(String[] args) {
-		System.out.println("hello hi");
-		System.out.println(1);
 	}
-}}
+}
