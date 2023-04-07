9246b05b41e2fc3e5ec122f53aa038c798d15836
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-04-07 23:36:13 +0530

    random update

--- JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/Main.java
+++ JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/Main.java
@@ -1,8 +1,5 @@
 public class Main { // inside this block of {} is the int main of java as found in cpp
-	public static void main(String[] args) { /*
-												 * Main function is the entry point of the program it has to be there
-												 * with the name main
-												 */
+	public static void main(String[] args) { //Main function is the entry point of the program it has to be there with the name main
 		System.out.println("hello world!!");
 	}
 }
