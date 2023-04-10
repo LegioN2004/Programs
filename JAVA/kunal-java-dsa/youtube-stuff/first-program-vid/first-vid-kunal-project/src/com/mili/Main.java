d8e236e720d6ec53ff7ea5e52c4b192258eb410b
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-04-10 20:09:27 +0530

    added new org and java stuff

--- JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/Main.java
+++ JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/Main.java
@@ -2,8 +2,14 @@ package com.mili; // com.mili it's the folder in which the java file will lie
 
 // more info stuff in the org notes of java
 
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("using the aliases or shortcut written code");
-	}
+    public static void main(String[] args) {
+        System.out.print("hello world"); //print doesn't add a new line in the output
+        System.out.println("a new line will be added after this");
+        System.out.println("using the aliases or shortcut written code"); // println prints a line and creates a new line
+        System.out.println(10274); // doesn't need apostrophes for numbers
+        Scanner input = new Scanner(System.in); // taking input look into note2.org for more details
+    }
 }
