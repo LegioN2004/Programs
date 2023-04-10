c50f8a3cf3e5d526743e485821b54daf0f57ef7b
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-04-10 16:35:14 +0530

    hello hi

--- JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com.mili/Main.java
+++
@@ -1,8 +0,0 @@
-package com.mili; // com.mili it's the folder in which the java file will lie
-import java.util.Arrays;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("using the aliases or shortcut written code");
-    }
-
-}
--- JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/Main.java
+++ JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/Main.java
@@ -1,7 +1,9 @@
 package com.mili; // com.mili it's the folder in which the java file will lie
 
+// more info stuff in the org notes of java
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("using the aliases or shortcut written code");
-    }
+	public static void main(String[] args) {
+		System.out.println("using the aliases or shortcut written code");
+	}
 }
