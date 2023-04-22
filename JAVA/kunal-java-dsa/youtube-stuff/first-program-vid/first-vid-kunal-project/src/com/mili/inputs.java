1a281d7ceef6452b390230d4e27db6f6d97c468a
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-04-22 21:23:35 +0530

    added java in the language section

--- JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/inputs.java
+++ JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/inputs.java
@@ -1,4 +1,12 @@
 package com.mili;
 
+import java.util.Scanner;
+
 public class inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // System.in takes keyb input
+        System.out.print("enter the input = "); // prints output
+        int rollno = input.nextInt(); // rollno takes value from keyb using input.nextInt
+        System.out.println("your roll no is " + rollno ); // + is used to join together the sentence string in "" as well as the value of the variable
+    }
 }
