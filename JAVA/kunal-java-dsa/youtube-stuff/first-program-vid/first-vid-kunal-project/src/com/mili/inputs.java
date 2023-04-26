a1cefde49c604a1353438a48fe29e8665af0268a
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-04-26 15:14:59 +0530

    new order of the files with the language name on the folders and other stuff aswell

--- JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/inputs.java
+++ JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/inputs.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class inputs {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); // System.in takes keyb input
+        Scanner input = new Scanner(System.in); // System.in takes keyb(in) input
         System.out.print("enter the input = "); // prints output
         int rollno = input.nextInt(); // rollno takes value from keyb using input.nextInt
         System.out.println("your roll no is " + rollno ); // + is used to join together the sentence string in "" as well as the value of the variable
+                                                          // '+' more on that later on how to convert types from one to another
+
+        int a = 10;
+
     }
 }
---
+++ JAVA/random-java-practices/first.java
@@ -0,0 +1,12 @@
+// program to add two numbers
+import java.util.Scanner;
+
+public class first {
+  public static void main (String[] args){
+	int a = 10;
+	Scanner inputno = new Scanner(System.in);
+	int b = inputno.nextInt();
+	System.out.print("the result of a + b is ");
+	System.out.println(a+b);
+  }
+}
