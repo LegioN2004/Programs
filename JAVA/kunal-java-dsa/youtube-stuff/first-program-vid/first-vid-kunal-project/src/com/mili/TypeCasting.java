861fb80825f470c11ca769c0b61f55f75d267601
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-04-29 14:10:32 +0530

    files added

--- JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/TypeCasting.java
+++ JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/TypeCasting.java
@@ -3,10 +3,13 @@ package com.mili;
 import java.util.Scanner;
 
 public class TypeCasting {
-    public static  void main(String[] args){
-        // some conditions need to be met to auto convert types , see note2.org
-        Scanner num = new Scanner(System.in);
-        float input = num.nextFloat();
-        System.out.println(input);
-    }
+  public static  void main(String[] args){
+	// some conditions need to be met to auto convert types , see note2.org
+	// Scanner num = new Scanner(System.in);
+	// float input = num.nextFloat();
+	// System.out.println(input);
+
+	int num = (int)(89.5f);
+	System.out.println(num);
+  }
 }
