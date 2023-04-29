d7753bd928567f3df90c9a834634625e6d6735e9
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-04-29 09:01:42 +0530

    going javaaaaaaaaaaaa

---
+++ JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/TypeCasting.java
@@ -0,0 +1,12 @@
+package com.mili;
+
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static  void main(String[] args){
+        // some conditions need to be met to auto convert types , see note2.org
+        Scanner num = new Scanner(System.in);
+        float input = num.nextFloat();
+        System.out.println(input);
+    }
+}
--- JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/sum2nos.java
+++ JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/com/mili/sum2nos.java
@@ -3,14 +3,14 @@ package com.mili;
 import java.util.Scanner;
 
 public class sum2nos {
-    public static  void main(String[] args){
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int a = 10;
         int b = 100;
-        int c = input.nextInt();
-
-        int sum = a + b - c;
-
-        System.out.println("Sum is " + sum + " byee " + "bhaaaaaaaaag saaale run hogya hai ab kya dekh rha hai");
+        float c = input.nextFloat(); //remember whenever you change the declaration datatype,
+        // also change the next____() <- datatype here as it defines which type of datatype values to take
+        float sum = a + b - c;
+        // after getting the output we see the value as 105.0 ( for c = 5), after a int input we get the value converted to float, this is type conversion and type casting see note2.org
+        System.out.println("Sum is " + sum + " byee " + "run hogya hai ab kya dekh rha hai phli fursat mein nikal bhaaaaaaaaag ");
     }
 }
