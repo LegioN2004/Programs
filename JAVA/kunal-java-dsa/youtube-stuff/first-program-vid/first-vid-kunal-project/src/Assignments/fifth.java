e0f8175603ddaa502717ee60cd68088b7e2e1dc6
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-05-12 23:48:46 +0530

    hahahaha

--- JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/Assignments/fifth.java
+++ JAVA/kunal-java-dsa/youtube-stuff/first-program-vid/first-vid-kunal-project/src/Assignments/fifth.java
@@ -3,16 +3,19 @@ package Assignments;
 import java.util.Scanner;
 
 public class fifth {
-    public static void main(String[] args) {
-        Scanner no = new Scanner(System.in);
-        System.out.print("print the value of a ");
-        double a = no.nextDouble();
-        System.out.print("print the value of b ");
-        double b = no.nextDouble();
-        if (a > b)
-            System.out.println("a is greater than b");
-        else
-            System.out.println("b is greater than a");
-        System.out.println("finished");
+  public static void main(String[] args) {
+    Scanner no = new Scanner(System.in);
+    System.out.print("print the value of a ");
+    double a = no.nextDouble();
+    System.out.print("print the value of b ");
+    double b = no.nextDouble();
+    if (a > b){
+      System.out.println("a is greater than b");
+    } else if (a == b){
+      System.out.println("a is equal to b");
+    } else {
+      System.out.println("b is greater than a");
     }
+    System.out.println("finished");
+  }
 }
