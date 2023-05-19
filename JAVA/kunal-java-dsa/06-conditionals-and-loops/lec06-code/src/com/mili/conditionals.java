ef78762cbdb01f6cd921dcb8a260c401962ffbb0
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-05-19 11:39:43 +0530

    updated readmes of folders

--- JAVA/kunal-java-dsa/06-conditionals-and-loops/lec06-code/src/com/mili/conditionals.java
+++ JAVA/kunal-java-dsa/06-conditionals-and-loops/lec06-code/src/com/mili/conditionals.java
@@ -1,20 +1,20 @@
-// if conditions
+// if-else conditions
 package com.mili;
 
 import java.util.Scanner;
 
-public class Main {
+public class conditionals {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
         long salary = a.nextLong();
         // using nested if else statments ignore line 10 and line 18 to 23
         if ( salary < 100000 ) {
             if (salary > 10000) {
-                salary += 2000; // salary = salary + 2000;
-            } else if (salary > 20000) {
+                salary += 2000; // same as -> salary = salary + 2000; 
+            } else if (salary > 20000) { // multiple else if statments
                 salary += 3000;
             } else {
-                salary += 1000;
+                salary += 1000; // if none of the above conditions are true then execute this condition
             }
             System.out.println("too less salary, you suck get the hell outta here and get a better job");
         }
---
+++ JAVA/kunal-java-dsa/06-conditionals-and-loops/lec06-code/src/com/mili/loops.java
@@ -0,0 +1,11 @@
+// loops 
+package com.mili;
+
+import java.util.Scanner;
+
+public class conditionals {
+    public static void main(String[] args) {
+        // Q1 : Print the numbers from 1 to 5
+        Scanner a = new Scanner(System.in);
+    }
+}
