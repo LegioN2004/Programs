7dbfb0c35b126bd5d8148f1c2b31f6ecae6154f8
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2024-02-13 17:09:37 +0530

    fixed some stuff in config files

---
+++ C/uni-stuff/semester-2/Programming-in-C/1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  int a;
+  printf("enter the number A = ");
+  scanf("%d", &a);
+  printf("The value is %d \n", a);
+  return 0;
+}
---
+++ C/uni-stuff/semester-2/Programming-in-C/2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int a, b;
+  printf("enter the number A = ");
+  scanf("%d", &a);
+  printf("enter the number B = ");
+  scanf("%d", &b);
+  int c = a + b;
+  printf("The value is %d \n", c);
+  return 0;
+}
