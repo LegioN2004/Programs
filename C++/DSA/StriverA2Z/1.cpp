0c4541d8ddb30e5b5880960c238c16f827c5a942
LegioN2004 <milindabarua04@gmail.com>
2024-02-25 15:53:41 +0530

    backup commit

---
+++ C++/DSA/StriverA2Z/1.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout << "heloo";
+    cin >> a >> b;
+    cout << a << b;
+    return 0;
+}
---
+++ C/uni-stuff/semester-2/Programming-in-C/hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main() {
+	printf("annoying isn't it, setting up everything and all \n");
+	printf("you can install these extensions paired with gcc and you are good to go");
+}
