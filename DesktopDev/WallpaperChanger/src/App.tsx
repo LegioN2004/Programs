489503d052f122e7917c2414c604fe28e25420c3
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2024-04-22 16:15:55 +0530

    first desktop app made, lesssssssssss gooooooooooooooooooooooooooooo

---
+++ DesktopDev/WallpaperChanger/src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="bg-gray-500">
+      <div className="font-bold text-3xl text-black">
+        Hi my name is Milinda Barua
+      </div>
+      <div className="text-red-300">
+        Tauri is probably the best and fast framework to get started with
+        building desktop apps without electron
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+        laborum officia recusandae blanditiis eius facilis? Praesentium autem
+        obcaecati fugiat mollitia, voluptatibus, iusto deleniti, ipsa molestias
+        recusandae asperiores dolore architecto perspiciatis. Nobis quae ab
+        facere amet. Temporibus obcaecati fuga quos doloribus eos, laudantium
+        cum facilis vel officia sequi eum molestiae soluta molestias repellat
+        doloremque! Atque cumque earum facilis nesciunt facere! Animi.{" "}
+      </p>
+    </div>
+  );
+}
+
+export default App;
