3a6b4bd5f666238072b7a90a9abf476d28405dd4
LegioN2004 <milindabarua04@gmail.com>
2024-06-02 16:29:04 +0530

    finally correct config file done

---
+++ C++/A2Z-DSA/basics/first.c
@@ -0,0 +1,11 @@
+// give me a code to input and output and show them in C
+#include <stdio.h>
+
+int main() {
+  int a, b;
+  printf("Hello World\n");
+  scanf("%d%d\n", &a, &b);
+  printf("fk u a hundred thousand times vscode %d %d", a, b);
+  printf("\nfk u a hundred thousand times vscode %d %d", a, b);
+  return 0;
+}
---
+++ C++/A2Z-DSA/basics/first.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  cout << "Hello World";
+  cin >> a >> b;
+  cout << a << " fk u a hundred thousand times vscode " << b;
+  return 0;
+}
