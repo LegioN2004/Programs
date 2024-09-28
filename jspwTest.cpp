b8a67dc4d43380c9667e466fca993bb2814f12ee
LegioN2004 <milindabarua04@gmail.com>
2024-09-28 09:25:29 +0530

    committtttttttt

--- jspwTest.cpp
+++ jspwTest.cpp
@@ -1,3 +1,16 @@
-\begin{document}
-\section{Dibrugarh University $aoirnt$}
-\end{document}
+\begin{table}
+    \centering
+    \begin{tabular}{C|C|C}
+         \hline
+         first & second & third\\
+         \hline
+         A & B & C\\
+         \hline
+         gargi & my & love\\
+         \hline
+         Love & her & lots\\
+         \hline
+    \end{tabular}
+    \caption{Caption}
+    \label{tab:my_label}
+\end{table}
