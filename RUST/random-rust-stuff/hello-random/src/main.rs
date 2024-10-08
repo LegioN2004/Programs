71832029ccc68a4f6155418c1832812bc8308c96
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2024-10-08 10:58:32 +0530

    today's assignment

---
+++ RUST/random-rust-stuff/hello-random/src/main.rs
@@ -0,0 +1,30 @@
+use std::i16;
+
+fn main() {
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    println!("Hello, world!");
+    let x: i8 = 10;
+    print!("{}", x);
+}
