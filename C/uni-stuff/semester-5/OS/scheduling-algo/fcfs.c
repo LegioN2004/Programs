869a6a67b5bfdd7b193f7bd1c06e8e7d892ff04f
legion2004 <milindabarua04@gmail.com>
2025-12-24 13:03:40 +0530

    done the three schudling algorithms

---
+++ C/uni-stuff/semester-5/OS/scheduling-algo/fcfs.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define max 30
+
+void main() {
+  int i, j;       // for using in the for loops
+  int n;          // for no of process the user wants to run.
+  int bt[max];    // burst time
+  int wt[max];    // waiting time
+  int tat[max];   // turn around time
+  float awt = 0;  // average waiting time
+  float atat = 0; // average turn around time
+
+  printf("enter the number of process: ");
+  scanf("%d", &n);
+  printf("enter the burst time of the process: ");
+  for (i = 0; i < n; i++) {
+    scanf("%d", &bt[i]);
+  }
+  printf("process\t burst time\t waiting time\t turn around time\n");
+  // loop run karwayenge to check process individually
+  for (i = 0; i < n; i++) {
+    wt[i] = 0;
+    tat[i] = 0;
+    // when we get a partciular job, it'll come according to the burst time and
+    // execute accordingly. prev job wt + bt
+    for (j = 0; j < i; j++)
+      wt[i] = wt[i] + bt[j];
+    tat[i] = wt[i] + bt[i];
+    awt = awt + wt[i]; // numerator part, sum of observations
+    atat = atat + tat[i];
+    printf("%d\t%d\t\t%d\t\t%d\n", i + 1, bt[i], wt[i], tat[i]);
+  }
+
+  // div by no of observations
+  awt = awt / n;
+  atat = atat / n;
+  printf("Average waiting time = %f", awt); // use %s for float type
+  printf("\nAverage turn around time = %f", atat);
+}
---
+++ C/uni-stuff/semester-5/OS/scheduling-algo/priority.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdio.h>
+void swap(int *a, int *b)
+{
+  int temp = *a;
+  *a = *b;
+#include <stdio.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    #include <stdio.h>
+
+    void swap(int *a, int *b)
+    {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+
+    int main(void)
+    {
+        int n;
+        printf("Enter Number of Processes: ");
+        if (scanf("%d", &n) != 1) {
+            fprintf(stderr, "Invalid input.\n");
+            return 1;
+        }
+
+        int b[n], p[n], index[n];
+        for (int i = 0; i < n; i++) {
+            /* Read burst and priority separately to avoid scanf confusion */
+            printf("Enter Burst Time for Process %d: ", i + 1);
+            while (scanf("%d", &b[i]) != 1) {
+                int c; while ((c = getchar()) != '\n' && c != EOF) ;
+                printf("Invalid. Enter Burst Time for Process %d: ", i + 1);
+            }
+
+            printf("Enter Priority Value for Process %d: ", i + 1);
+            while (scanf("%d", &p[i]) != 1) {
+                int c; while ((c = getchar()) != '\n' && c != EOF) ;
+                printf("Invalid. Enter Priority Value for Process %d: ", i + 1);
+            }
+
+            index[i] = i + 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int a = p[i], m = i;
+            for (int j = i; j < n; j++) {
+                if (p[j] > a) { a = p[j]; m = j; }
+            }
+            swap(&p[i], &p[m]);
+            swap(&b[i], &b[m]);
+            swap(&index[i], &index[m]);
+        }
+
+        int t = 0;
+        printf("Order of process Execution is\n");
+        for (int i = 0; i < n; i++) {
+            printf("P%d is executed from %d to %d\n", index[i], t, t + b[i]);
+            t += b[i];
+        }
+
+        printf("\nProcess Id     Burst Time   Wait Time    TurnAround Time\n");
+        int wait_time = 0;
+        for (int i = 0; i < n; i++) {
+            printf("P%d          %d          %d          %d\n", index[i], b[i], wait_time, wait_time + b[i]);
+            wait_time += b[i];
+        }
+
+        return 0;
+    }
