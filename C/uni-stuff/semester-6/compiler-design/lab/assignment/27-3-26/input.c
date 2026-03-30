5c3d3e543603996ccfebfd90321a9829c58cc637
legion2004 <milindabarua04@gmail.com>
2026-03-30 08:28:06 +0530

    lab assignment done

---
+++ C/uni-stuff/semester-6/compiler-design/lab/assignment/27-3-26/input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// This is a single line comment
+
+int main() {
+  int a = 5; /* This is a multi-line comment */
+  int b = 10;
+
+  // Another comment
+  printf("%d", a + b);
+}
---
+++ C/uni-stuff/semester-6/compiler-design/lab/assignment/27-3-26/output.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+
+int main() {
+  int a = 5; 
+  int b = 10;
+
+  
+  printf("%d", a + b);
+}
