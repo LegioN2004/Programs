9655fd13aa9a3d7eb0ea7048f41dbc0183d30ab6
legion2004 <milindabarua04@gmail.com>
2026-03-30 08:28:24 +0530

    lab insem assignment done

---
+++ C/uni-stuff/semester-6/compiler-design/lab/assignment/30-3-26/input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// This is a single line comment
+int main() {
+  /* This is a
+     multi-line comment */
+  char *s = "hello // not a comment";
+  printf("hi\n"); // end comment
+  return 0;
+}
---
+++ C/uni-stuff/semester-6/compiler-design/lab/assignment/30-3-26/output.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  
+
+  char *s = "hello // not a comment";
+  printf("hi\n"); 
+  return 0;
+}
