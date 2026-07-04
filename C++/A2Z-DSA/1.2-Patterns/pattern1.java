7a177133c69e910ed0df3024cdfbbc35c7f8ce86
legion2004 <milindabarua04@gmail.com>
2026-07-04 10:48:23 +0530

    Add pattern generation program in Java

---
+++ C++/A2Z-DSA/1.2-Patterns/pattern1.java
@@ -0,0 +1,81 @@
+public class pattern1 {
+
+    static void p1() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void p2() {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void p3() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    static void p4() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    static void p5() {
+        // simple way
+        // for (int i = 5; i >= 1; i--) {
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print("*");
+        // }
+        // System.out.println();
+        // }
+
+        // thoda sa jeng way
+        for (int i = 0; i < 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    static void p6() {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9 - i + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i * 2 + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void p7() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        p6();
+    }
+}
